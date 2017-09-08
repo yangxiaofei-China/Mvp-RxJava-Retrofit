5932ba57734974db56caaa64ac7d732361d0d93f
钟家宝 <zhongjiaobao@hjtechcn.cn>
2017-09-08 11:47:31 +0800

    修改分包

---
+++ app/src/main/java/com/zhon/frame/RootActivity.java
@@ -0,0 +1,21 @@
+package com.zhon.frame;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/*
+ * 项目名:    Mvp-RxJava-Retrofit
+ * 包名       com.zhon.frame
+ * 文件名:    RootActivity
+ * 创建者:    ZJB
+ * 创建时间:  2017/9/8 on 11:41
+ * 描述:     TODO 闪屏页面
+ */
+public class RootActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_root);
+    }
+}
